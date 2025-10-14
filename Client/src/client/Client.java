6584d7b5212b9fe93b6cf2dc81e9e03969c6e9d3
LABANANEBINJAUNE <tristan.bleau@etud.polymtl.ca>
2025-10-14 16:54:18 -0400

    client print

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -18,7 +18,7 @@ public class Client {
 		String ip = ipv.getIP();
 		int port = ipv.getPort();
 		Scanner scanner = new Scanner(System.in);
-	
+		
 		
 		// Création d'une nouvelle connexion aves le serveur
 		socket = new Socket(ip, port);
@@ -26,13 +26,14 @@ public class Client {
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		// Attente de la réception d'un message envoyé par le, server sur le canal
-		
+		System.out.print(in.readUTF());	
 		// fermeture de La connexion avec le serveur
 		while(true)
 		{
 			System.out.print(">");
 			cmdHandler.lastcmd = scanner.nextLine().toUpperCase();
 			cmdHandler.handleOut(in, out, socket);
+			System.out.println(in.readUTF());
 			
 		}
 	}
