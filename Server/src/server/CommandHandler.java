98191b40ceba34c1d42d10fdbb67c9166c0e16ac
LeGoatier <lejustingauthier@gmail.com>
2025-10-14 15:58:27 -0400

    début structure commande coté serveur (ne compile pas)

--- Server/src/server/CommandHandler.java
+++ Server/src/server/CommandHandler.java
@@ -4,48 +4,73 @@ import java.io.IOException;
 import java.io.DataInputStream;
 import java.io.File;
 public class CommandHandler {
-	String wd = "";
+	File currentDirectory = new File("");
+	
 	public void handleCmd(String input, DataInputStream in, DataOutputStream out) {
 		try {
-		String[] words = input.split(" ");
-		switch(words[0]) {
-		case "upload":
-			out.writeUTF("ACK-UPLOAD");
-			in.readAllBytes();
-			break;
-		case "download":
-			break;
-		case "echo":
-			String s = "";
-			for(int i = 1; i < words.length; ++i) {
-				s.concat(words[i].concat(" "));
-				out.writeUTF(s.concat("\n"));
-			}
-			break;
-		case "cd":
-			if(new File(wd.concat(words[1])).exists()) {
-				wd += words[1];
-			}
-			break;
-		case "ls":
-			break;
-		case "mkdir":
-			File folder = new File(wd.concat(words[1]));
-			if(!folder.exists()) {
-				folder.mkdir();
-				out.writeUTF("\033[93mFolder created\033[0m");
-			}
-			else {
-				out.writeUTF("\033[91mFolder already Exists\033[0m");
-			}
-			break;
-		case "delete":
-			break;
-		case "exit":
-			break;
-		}
+			String[] words = input.split(" ");
+			
+			String cmd = words[0].toUpperCase();
+			String argument = words[1];
+	
+		    switch (cmd) {
+		        case "CD":
+		            if (words.length < 2) {
+		                out.writeUTF("Utilisation: cd <répertoire>\n");
+		            } else {
+		                changeDirectory(argument, out);
+		            }
+		            break;
+	
+		        case "LS":
+		            listFiles(out);
+		            break;
+		            
+		        case "MKDIR":
+		        		makeDirectory(argument, out);
+		        		break;
+	
+		        case "UPLOAD":
+		            receiveFile(argument, out);
+		            break;
+	
+		        case "DOWNLOAD":
+		            sendFile(argument, out);
+		            break;
+	
+		        case "DELETE":
+		            deleteFile(argument, out);
+		            break;
+	
+		        case "EXIT":
+		            exit(out);
+		            break;
+		            
+		        case "ECHO":
+					String s = "";
+					for(int i = 1; i < words.length; ++i) {
+						s.concat(words[i].concat(" "));
+						out.writeUTF(s.concat("\n"));
+					}
+					break;
+					
+		        default:
+		            out.writeUTF("Commande inconnue\n");
+		    }			
 		}catch(IOException e) {
 			System.out.println("IO EXCEPTION");
 		}
+	}
+
+	private void listFiles(DataOutputStream out) {
+        File[] files = currentDirectory.listFiles();
+        if (files == null || files.length == 0) {
+            out.writeUTF("Aucun fichier dans ce répertoire\n");
+        } else {
+            for (File f : files) {
+                out.write(f.getName() + "\n");
+            }
+        }
+		
 	}
 }
