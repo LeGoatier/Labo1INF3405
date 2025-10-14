ad532159c4edfe16b04ff497362a6da849e48a42
plankton <nathan.deziel@polymtl.ca>
2025-10-14 15:38:51 -0400

    scoffed

--- Server/src/server/CommandHandler.java
+++ Server/src/server/CommandHandler.java
@@ -2,6 +2,7 @@ package server;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.DataInputStream;
+import java.io.File;
 public class CommandHandler {
 	String wd = "";
 	public void handleCmd(String input, DataInputStream in, DataOutputStream out) {
@@ -22,21 +23,26 @@ public class CommandHandler {
 			}
 			break;
 		case "cd":
+			if(new File(wd.concat(words[1])).exists()) {
+				wd += words[1];
+			}
 			break;
 		case "ls":
 			break;
 		case "mkdir":
+			File folder = new File(wd.concat(words[1]));
+			if(!folder.exists()) {
+				folder.mkdir();
+				out.writeUTF("\033[93mFolder created\033[0m");
+			}
+			else {
+				out.writeUTF("\033[91mFolder already Exists\033[0m");
+			}
 			break;
 		case "delete":
 			break;
 		case "exit":
 			break;
-		}
-		if(words[0] == "upload") {
-			
-		}
-		else if (words[0] == "echo") {
-			
 		}
 		}catch(IOException e) {
 			System.out.println("IO EXCEPTION");
