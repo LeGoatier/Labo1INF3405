0ce90157b80f218dc0a8805593c3dd6d202e5fcc
LABANANEBINJAUNE <tristan.bleau@etud.polymtl.ca>
2025-10-14 14:52:50 -0400

    client com test

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -15,7 +16,7 @@ public class Client {
 		IpValidater ipv = new IpValidater();
 		String ip = ipv.getIP();
 		int port = ipv.getPort();
-		
+		Scanner scanner = new Scanner(System.in);
 	
 		
 		// Création d'une nouvelle connexion aves le serveur
@@ -23,10 +24,16 @@ public class Client {
 		System.out.format("Serveur lancé sur [%s:%d]", ip, port);
 		// Céatien d'un canal entrant pour recevoir les messages envoyés, par le serveur
 		DataInputStream in = new DataInputStream(socket.getInputStream());
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		// Attente de la réception d'un message envoyé par le, server sur le canal
-		String helloMessageFromServer = in.readUTF();
-		System.out.println(helloMessageFromServer);
+		
 		// fermeture de La connexion avec le serveur
-		socket.close();
+		while(true)
+		{
+			System.out.println(in.readUTF() + "\n");
+			String output = scanner.nextLine();
+			out.writeUTF(output);
+			
+		}
 	}
 }
